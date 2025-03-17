c8e882097b45ad631582851268047294d6e9af15
Harun <ahmetharuncakir5600@gmail.com>
2025-03-17 11:05:46 +0300

    torch multi-class classification

--- torch/wine.py
+++ torch/wine.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -77,4 +79,58 @@ y_test_tensor = torch.tensor(y_test, dtype=torch.long)
 # Display example tensors
 print("Sample of X_train_tensor:", X_train_tensor[0])
 print("Sample of y_train_tensor:", y_train_tensor[0])
+
+model = nn.Sequential(
+    nn.Linear(13, 10),
+    nn.ReLU(),
+    nn.Linear(10, 10),
+    nn.ReLU(),
+    nn.Linear(10, 3)
+)
+
+print(model)
+'''
+Sequential(
+  (0): Linear(in_features=13, out_features=10, bias=True)
+  (1): ReLU()
+  (2): Linear(in_features=10, out_features=10, bias=True)
+  (3): ReLU()
+  (4): Linear(in_features=10, out_features=3, bias=True)
+)
+'''
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Train the model
+num_epochs = 150
+history = {'loss': [], 'val_loss': []}
+for epoch in range(num_epochs):
+    model.train()
+    optimizer.zero_grad()
+    outputs = model(X_train)
+    loss = criterion(outputs, y_train)
+    loss.backward()
+    optimizer.step()
+    history['loss'].append(loss.item())
+
+    model.eval()
+    with torch.no_grad():
+        outputs_val = model(X_test)
+        val_loss = criterion(outputs_val, y_test)
+        history['val_loss'].append(val_loss.item())
+
+    if (epoch + 1) % 10 == 0:
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}, Validation Loss: {val_loss.item():.4f}')
+
+'''
+Epoch [10/150], Loss: 1.1324, Validation Loss: 1.1123
+Epoch [20/150], Loss: 1.1020, Validation Loss: 1.0844
+Epoch [30/150], Loss: 1.0713, Validation Loss: 1.0547
+Epoch [40/150], Loss: 1.0366, Validation Loss: 1.0204
+...
+Epoch [150/150], Loss: 0.3014, Validation Loss: 0.3216
+'''
+
 
